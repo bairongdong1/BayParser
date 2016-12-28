66d9632a51df0953ed8e8d0bffa2469fef13e929
白荣东 <bairongdong@admaster.com.cn>
2016-12-28 11:23:52 +0800

    delete code not need.

--- src/main/java/bayparser/BayParser.java
+++ src/main/java/bayparser/BayParser.java
@@ -1,50 +1,28 @@
-/*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
 package bayparser;
 
 import java.io.IOException;
 
-import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.queryparser.surround.parser.QueryParser;
 import org.apache.lucene.queryparser.surround.query.BasicQueryFactory;
 import org.apache.lucene.queryparser.surround.parser.ParseException;
-import org.apache.lucene.index.Term;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
-import org.elasticsearch.index.query.QueryStringQueryParser;
 import org.elasticsearch.index.query.QueryParseContext;
 import org.elasticsearch.index.query.QueryParsingException;
 import org.elasticsearch.index.mapper.MappedFieldType;
 
-public class BayParser extends QueryStringQueryParser {
+public class BayParser implements org.elasticsearch.index.query.QueryParser {
 
     private final ESLogger logger;
 
     @Inject
     public BayParser(Settings settings) {
-        super(settings);
         logger = Loggers.getLogger(getClass(), settings);
     }
 
@@ -81,7 +59,7 @@ public class BayParser extends QueryStringQueryParser {
             queryString= parser.text();
             // move to the next token
             parser.nextToken();
-            logger.info("CCCCCC" + queryString);
+            logger.info("query string for bay_parser is " + queryString);
         }
 
         Query query = null;
